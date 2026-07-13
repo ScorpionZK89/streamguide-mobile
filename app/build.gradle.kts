205a7e7f8c033ef3d3695eaf742b851029223467
Mark <markstavasius89@gmail.com>
2026-07-13 18:21:30 +0200

    Update Android media route runtime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation(libs.androidx.media3.exoplayer)
     implementation(libs.androidx.media3.cast)
     implementation(libs.androidx.media3.ui)
+    implementation(libs.androidx.mediarouter)
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.work.runtime.ktx)
